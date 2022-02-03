2a7b5340fadb6fe033a6d001861d7788ecde7c5d
Jorge Mendez <mendezjorge98@gmail.com>
2022-02-03 15:00:16 -0300

    make components for weatherApp

--- src/app/weather-app/weather-app.module.ts
+++ src/app/weather-app/weather-app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherComponent } from './weather.component';
+import { SearchComponent } from './search/search.component';
+import { InfoComponent } from './info/info.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 
 @NgModule({
   declarations: [
-    WeatherComponent
+    WeatherComponent,
+    SearchComponent,
+    InfoComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule
