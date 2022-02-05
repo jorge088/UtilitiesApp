6304ae712a73ba1ecf4e3ce78fd1118149b0db93
Jorge Mendez <mendezjorge98@gmail.com>
2022-02-05 05:29:50 -0300

    fix styles

--- src/app/weather-app/weather.component.ts
+++ src/app/weather-app/weather.component.ts
@@ -12,8 +12,7 @@ export class WeatherComponent implements OnInit {
   weatherInfo:any=[];
   city:string = "San salvador de Jujuy";
   appBackColor:string="";
-  @HostBinding('style.background-color')  color !: string;
-  //@HostBinding('class.backgroundImgCold')  coldWeather = false;
+  @HostBinding('style.background-image')  image !: string;
 
   constructor( private weather:WeatherService ) { 
   }
@@ -21,24 +20,26 @@ export class WeatherComponent implements OnInit {
   ngOnInit(): void {
     this.getCurrentPosition();
   }
+
   getWeatherInfo(value:string){
     this.weather.getFromUserSelection(value).subscribe(data=>{
       this.weatherInfo = data;
       this.weatherInfo.main.temp = String(parseInt(this.weatherInfo.main.temp) - 273);
       this.weatherInfo.main.temp_max = String(parseInt(this.weatherInfo.main.temp_max)-273);
       this.weatherInfo.main.temp_min = String(parseInt(this.weatherInfo.main.temp_min)-273);
-      setTimeout(()=>{
-        this.getStyle(this.weatherInfo);
-      },100); 
     });
+    setTimeout(()=>{
+      this.getStyle(this.weatherInfo);
+    },300); 
   }
+
   getStyle(data:any){
     if(parseInt(data.main.temp)<16){
       this.appBackColor="#3053AE";
-      this.color="blue";
+      this.image="url('../../assets/cold.jpg')";
     }else if(parseInt(data.main.temp)>=16){
       this.appBackColor="#ff9800";
-      this.color="red"
+      this.image="url('../../assets/warm.jpg')";
     } 
   }
   getCurrentPosition(){
